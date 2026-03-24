5f6cf594eb198149d1637a6143bae3bb33ac2365
snaksartrate <sanskartarte080307@gmail.com>
2026-03-24 12:42:48 +0530

    designed board and allowed notation based initialization

---
+++ constants.py
@@ -0,0 +1,3 @@
+board_size = 9
+komi = 6.5
+valid_notation_chars = ('_', 'w', 'b')
---
+++ environment.py
@@ -0,0 +1,22 @@
+from constants import komi, board_size, valid_notation_chars
+
+def is_valid_notation(notation : str, board_size=board_size) -> bool:
+    if len(notation) != board_size * board_size:
+        return False
+    return set(notation).issubset(valid_notation_chars)
+
+class Board:
+    def __init__(self, notation=None):
+        self.grid = [0 for _ in range(board_size * board_size)]
+        self.notation = '_' * board_size * board_size if not notation else notation
+        if notation and is_valid_notation(notation):
+            mp = {'_' : 0, 'b' : 1, 'w' : 2}
+            for i in range(board_size * board_size):
+                self.grid[i] = mp[notation[i]]
+
+class Position:
+    def __init__(self):
+        self.board = Board()
+        self.score = -komi
+        self.black_to_play = True
+        self.eval = 0
