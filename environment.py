9f8f3555a0d113cb5fdbdfdafdeb098957045b38
snaksartrate <sanskartarte080307@gmail.com>
2026-03-29 16:47:30 +0530

    added unit as a data structure, its liberties n stuff, but gotta fix make a move function

--- environment.py
+++ environment.py
@@ -21,6 +21,13 @@ class Board:
             s += C.val[num + 1] # -1 corresponds to white on the grid, 0 to empty square, and 1 to black
         return s
 
+class Unit:
+    def __init__(self, unit : set):
+        self.unit = unit
+        self.liberties = set()
+        self.liberty_count = 0
+        self.colour_is_black = None
+
 class Position:
     def __init__(self, notation = None, parent = None):
         self.board = Board(notation)
@@ -28,9 +35,12 @@ class Position:
         self.black_to_play = True
         self.eval = 0
         self.parent = parent
-        self.children = []
-        self.liberties = 0
+        self.children = set()
+        self.liberties = set()
+        self.liberty_count = 0
         self.ko = None
+        self.black_units = set()
+        self.white_units = set()
 
     def get_notation(self) -> str:
         return self.board.get_notation()
--- movegen.py
+++ movegen.py
@@ -1,22 +1,32 @@
-from environment import Position, Board
+from copy import deepcopy
 
-def get_pseudo_legal(board : Board, black_to_play : bool) -> list:
-    pseudo_legal = [board] # if the player decides to pass
-    stone = 1 if black_to_play else 2
+from environment import Position, Board, Unit
+from eval import get_units
+
+def get_pseudo_legal(board : Board) -> list:
+    pseudo_legal = [None] # if the player decides to pass
     board_size = len(board.grid)
     for i in range(board_size * board_size):
         if not board.grid[i]:
-            board.grid[i] = stone
-            pseudo_legal.append(board.copy())
-            board.grid[i] = 0
+            pseudo_legal.append(i)
     return pseudo_legal
 
-def validate(pseudo_legal : list, black_to_play : bool) -> list:
-    for i in range(len(pseudo_legal)):
+def make_a_move(position : Position, move : int, black_to_play : bool) -> Position: # remove this function later. it is so fucking redundant.
+    position[move] = 1 if black_to_play else -1                                     # wait, what the fuck did i write?
+
+def validate(position : Position, pseudo_legal : list, black_to_play : bool) -> list:
+    # check for pseudo_legal[0] -> the "pass" move
+    for i in range(1, len(pseudo_legal)):
         # perform validity checks, remove invalid moves
+        new_position = deepcopy(position)
+        new_position = make_a_move(new_position, pseudo_legal[i], black_to_play)
+        black_units, white_units = get_units(new_position.board)
+        if black_to_play:
+            for unit in black_units:
+                if unit.l
         pass
     return pseudo_legal
 
 def move_gen(position : Position):
-    pseudo_legal = get_pseudo_legal(position.black_to_play, position.black_to_play)
-    moves = validate(pseudo_legal, position.black_to_play)
+    pseudo_legal = get_pseudo_legal(position.board)
+    moves = validate(position, pseudo_legal, position.black_to_play)
