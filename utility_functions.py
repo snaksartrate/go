98eca0a86da61dd28a1e8dd6d3efabceef070acf
snaksartrate <sanskartarte080307@gmail.com>
2026-04-03 23:21:32 +0530

    gotta restart, trading time for space and depth

--- utility_functions.py
+++ utility_functions.py
@@ -12,17 +12,18 @@ def adjacent(source : int) -> list[int]:
     limit = board_size * board_size
     return [a for a in adj if 0 <= a and a < limit]
 
-def dfs(grid : list, source : int) -> set:
+def dfs(grid : list, source : int) -> set[int]:
     colour = grid[source]
     unit = {source}
     stack = [source]
-    visited = {source}
+    visited = [False for _ in range(board_size * board_size)]
     curr = source
+    visited[source] = True
     while stack:
         curr = stack.pop()
         for adj in adjacent(curr):
-            if grid[adj] == colour and adj not in visited:
-                visited.add(adj)
+            if grid[adj] == colour and not visited[adj]:
+                visited[adj] = True
                 stack.append(adj)
                 unit.add(adj)
     return unit
